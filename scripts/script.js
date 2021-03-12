bb638599e4c9a85637834b76cfbb5393dbc7210d
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2021-03-12 14:11:41 +0400

    getData added, modal modified

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
+  const getData = (url, callback) => {
+    // const request = new XMLHttpRequest();
+    // request.open('GET', url);
+    // request.send();
 
+    // request.addEventListener('readystatechange', () => {
+    //   if(request.readyState !== 4) return;
+    //   if(request.status === 200) {
+    //     const response = JSON.parse(request.response);
+    //     callback(response);
+    //   } else {
+    //     console.log(new Error('Ошибка:', request.status));
+    //   }
+    // });
+
+    fetch(url)
+      .then((response) => {
+        if(response.ok) {
+          return response.json()
+        }
+        throw new Error(response.statusText)
+      })
+      .then(callback)
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+  
   const tabs = () => {
     const cardDetailChangeElems = document.querySelectorAll('.card-detail__change');
     const cardDetailsTitleElem = document.querySelector('.card-details__title');
@@ -99,21 +126,70 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const modal = () => {
     const cardDetailsButtonBuy = document.querySelector('.card-details__button_buy');
-    const modal = document.querySelector('.modal')
+    const cardDetailsBttonDelivery = document.querySelector('.card-details__button_delivery');
+    const modal = document.querySelector('.modal');
+    const cardDetailsTitle = document.querySelector('.card-details__title');
+    const modalTitle = modal.querySelector('.modal__title');
+    const modalSubtitle = modal.querySelector('.modal__subtitle');
+    const modalTitleSubmit = document.querySelector('.modal__title-submit')
 
-    cardDetailsButtonBuy.addEventListener('click', () => {
-      modal.classList.add('open')
-    });
-
-    modal.addEventListener('click', (event) => {
+    const openModal = event => {
       const target = event.target;
-      if(target.classList.contains('modal__close')) {
-        modal.classList.remove('open')
+      modal.classList.add('open');
+      document.addEventListener('keydown', escapeHandler);
+      modalTitle.textContent = cardDetailsTitle.textContent;
+      modalTitleSubmit.value = cardDetailsTitle.textContent;
+      modalSubtitle.textContent = target.dataset.buttonBuy;
+    };
+
+    const closeModal = () => {
+      modal.classList.remove('open');
+      document.removeEventListener('keydown', escapeHandler);
+    };
+
+    const escapeHandler = event => {
+      if(event.code === 'Escape') {
+        closeModal();
       }
+    };
+
+    cardDetailsButtonBuy.addEventListener('click', openModal);
+    modal.addEventListener('click', event => {
+      const target = event.target;
+      if(target.classList.contains('modal__close') || target === modal) {
+        closeModal();
+      }  
     });
+    cardDetailsBttonDelivery.addEventListener('click', openModal)
+    
   };
+
+  const renderCrossSell = () => {
+    const crossSellList = document.querySelector('.cross-sell__list');
+
+    const createCrossSellItem = (good) => {
+      const liItem = document.createElement('li');
+      liItem.innerHTML = `
+        <article class="cross-sell__item">
+          <img class="cross-sell__image" src="${good.photo}" alt="${good.name}">
+          <h3 class="cross-sell__title">${good.name}</h3>
+          <p class="cross-sell__price">${good.price}</p>
+          <button type="button" class="button button_buy cross-sell__button">Купить</button>
+        </article>
+      `;
+      return liItem;
+    }
+    const createCrossSellList = (goods) => {
+      goods.forEach(item => {
+        crossSellList.append(createCrossSellItem(item))
+      })
+    };
+
+    getData('cross-sell-dbase/dbase.json', createCrossSellList)
+  }
 
   tabs();
   accordeon();
   modal();
+  renderCrossSell();
 });
