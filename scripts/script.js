ed0887eef4eadb72d4d6d3b331fefa0263b26f03
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2021-03-13 15:18:46 +0300

    a-menu, goods added

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
+  const getData = (url, callback) => {
+    // const request = new XMLHttpRequest();
+    // request.open('GET', url);
+    // request.send();
 
+    // request.addEventListener('readystatechange', () => {
+    //   if(request.readyState !== 4) return;
+    //   if(request.status === 200) {
+    //     const response = JSON.parse(request.response);
+    //     callback(response);
+    //   } else {
+    //     console.log(new Error('Ошибка:', request.status));
+    //   }
+    // });
+
+    fetch(url)
+      .then((response) => {
+        if(response.ok) {
+          return response.json()
+        }
+        throw new Error(response.statusText)
+      })
+      .then(callback)
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+  
   const tabs = () => {
     const cardDetailChangeElems = document.querySelectorAll('.card-detail__change');
     const cardDetailsTitleElem = document.querySelector('.card-details__title');
@@ -99,21 +126,101 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const modal = () => {
     const cardDetailsButtonBuy = document.querySelector('.card-details__button_buy');
-    const modal = document.querySelector('.modal')
+    const cardDetailsBttonDelivery = document.querySelector('.card-details__button_delivery');
+    const modal = document.querySelector('.modal');
+    const cardDetailsTitle = document.querySelector('.card-details__title');
+    const modalTitle = modal.querySelector('.modal__title');
+    const modalSubtitle = modal.querySelector('.modal__subtitle');
+    const modalTitleSubmit = document.querySelector('.modal__title-submit')
 
-    cardDetailsButtonBuy.addEventListener('click', () => {
-      modal.classList.add('open')
-    });
-
-    modal.addEventListener('click', (event) => {
+    const openModal = event => {
       const target = event.target;
-      if(target.classList.contains('modal__close')) {
-        modal.classList.remove('open')
+      modal.classList.add('open');
+      document.addEventListener('keydown', escapeHandler);
+      modalTitle.textContent = cardDetailsTitle.textContent;
+      modalTitleSubmit.value = cardDetailsTitle.textContent;
+      modalSubtitle.textContent = target.dataset.buttonBuy;
+    };
+
+    const closeModal = () => {
+      modal.classList.remove('open');
+      document.removeEventListener('keydown', escapeHandler);
+    };
+
+    const escapeHandler = event => {
+      if(event.code === 'Escape') {
+        closeModal();
       }
+    };
+
+    cardDetailsButtonBuy.addEventListener('click', openModal);
+    modal.addEventListener('click', event => {
+      const target = event.target;
+      if(target.classList.contains('modal__close') || target === modal) {
+        closeModal();
+      }  
     });
+    cardDetailsBttonDelivery.addEventListener('click', openModal)
+    
   };
+
+  const renderCrossSell = () => {
+    const COUNT_ROW_GOODS = 4;
+
+    const crossSellList = document.querySelector('.cross-sell__list');
+    const crossSellAdd = document.querySelector('.cross-sell__add');
+    const allGoods = [];
+    let wrapRender = null;
+
+    const shuffle = arr => arr.sort(() => Math.random() - 0.5);
+    
+    const createCrossSellItem = ({ photo, name, price }) => {
+
+      const liItem = document.createElement('li');
+      liItem.innerHTML = `
+        <article class="cross-sell__item">
+          <img class="cross-sell__image" src="${photo}" alt="${name}">
+          <h3 class="cross-sell__title">${name}</h3>
+          <p class="cross-sell__price">${price}</p>
+          <button type="button" class="button button_buy cross-sell__button">Купить</button>
+        </article>
+      `;
+      return liItem;
+    };
+
+    const render = arr => {
+      arr.forEach(item => {
+        crossSellList.append(createCrossSellItem(item));
+      })
+    }
+
+    const wrapper = (fn, count) => {
+      let counter = 0;
+      return (...args) => {
+        if(counter === count) return;
+        counter++
+        return fn(...args)
+      }
+    };
+
+    const createCrossSellList = (goods) => {
+      wrapRender = wrapper(render, parseInt(goods.length/COUNT_ROW_GOODS) + 1);
+      allGoods.push(...shuffle(goods));
+      const fourItems = allGoods.splice(0, COUNT_ROW_GOODS);
+      render(fourItems);
+    };
+
+    crossSellAdd.addEventListener('click', () => {
+      wrapRender(allGoods.splice(0, COUNT_ROW_GOODS));
+      //crossSellAdd.remove();
+    })
+
+    getData('cross-sell-dbase/dbase.json', createCrossSellList)
+  }
 
   tabs();
   accordeon();
   modal();
+  renderCrossSell();
+  amenu('.header__menu', '.header-menu__list', '.header-menu__item', '.header-menu__burger');
 });
